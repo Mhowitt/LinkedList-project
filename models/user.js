b693702ea7e8f2ae9b30811e9cb008245b4d2bf6
Miranda Howitt <miranda.howitt@gmail.com>
2018-04-03 17:20:00 -0700

    user model and router

--- models/user.js
+++ models/user.js
@@ -1,6 +1,57 @@
 const mongoose = require("mongoose");
 const Company = require("./company");
 
-const userSchema = new mongoose.Schema({});
+const userSchema = new mongoose.Schema(
+  {
+    firstName: String,
+    lastName: String,
+    username: String,
+    email: String,
+    password: String,
+    currentCompany: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Company"
+    },
+    photo: String,
+    experience: {
+      jobTitle: String,
+      company: String,
+      startDate: Date,
+      endDate: Date
+    },
+    education: {
+      institution: String,
+      degree: String,
+      endDate: Date
+    },
+    skills: [String]
+  },
+  { timestamps: true } // automatically adds createdAt and updatedAt
+);
+
+/**
+ * Create a new user.
+ * @param {Object} newUser an instance of a user document
+ */
+
+userSchema.statics = {
+  createUser(newUser) {
+    return this.findOne({ username: newUser.username }).then(user => {
+      if (user) {
+        throw new Error(`The username ${newUser.username} already exists`);
+      }
+      return newUser
+        .save()
+        .then(user =>
+          Company.findOneAndUpdate(user.company, {
+            $addToSet: { employees: user._id }
+          }).then(() => user)
+        )
+        .catch(err => {
+          return Promise.reject(err);
+        });
+    });
+  }
+};
 
 module.exports = mongoose.model("User", userSchema);
--- routers/users.js
+++ routers/users.js
@@ -3,4 +3,19 @@ const router = express.Router();
 
 const { users } = require("../handers");
 
+router
+  .route("")
+  .get(users.readUsers)
+  .post(users.createUser);
+
+router.get("/new").get(users.newUserForm);
+
+router
+  .route("/:userId")
+  .get(users.readUser)
+  .patch(users.updateUser)
+  .delete(users.deleteUser);
+
+router.route("/:userId/edit").get(users.editUser);
+
 module.exports = router;
