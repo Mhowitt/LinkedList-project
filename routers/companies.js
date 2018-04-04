3d45b681fe5b4efe16d47aa50c01ebe510ef0412
Miranda Howitt <miranda.howitt@gmail.com>
2018-04-03 17:36:08 -0700

    routes done

--- routers/companies.js
+++ routers/companies.js
@@ -3,4 +3,19 @@ const router = express.Router();
 
 const { companies } = require("../handers");
 
+router
+  .route("")
+  .get(companies.readCompanies)
+  .post(companies.createCompany);
+
+router.get("/new").get(companies.newCompanyForm);
+
+router
+  .route("/:companyId")
+  .get(companies.readCompany)
+  .patch(companies.updateCompany)
+  .delete(companies.deleteCompany);
+
+router.route("/:companyId/edit").get(companies.editCompany);
+
 module.exports = router;
--- routers/jobs.js
+++ routers/jobs.js
@@ -3,4 +3,19 @@ const router = express.Router();
 
 const { jobs } = require("../handers");
 
+router
+  .route("")
+  .get(jobs.readJobPostings)
+  .post(jobs.createJobPosting);
+
+router.get("/new").get(jobs.newJobPostingForm);
+
+router
+  .route("/:jobId")
+  .get(jobs.readJobPosting)
+  .patch(jobs.updateJobPosting)
+  .delete(jobs.deleteJobPosting);
+
+router.route("/:jobId/edit").get(jobs.editJobPosting);
+
 module.exports = router;
