fba1881bef0067b573049b1211970bc07f3b8e0a
Miranda Howitt <miranda.howitt@gmail.com>
2018-04-03 20:50:57 -0700

    added bases for handler functions

--- handlers/companies.js
+++ handlers/companies.js
@@ -1,3 +1,17 @@
 const { Company } = require("../models");
 const Validator = require("jsonschema").Validator;
 const v = new Validator();
+
+function readCompanies(req, res, next) {}
+
+function createCompany(req, res, next) {}
+
+function newCompanyForm(req, res, next) {}
+
+function readCompany(req, res, next) {}
+
+function updateCompany(req, res, next) {}
+
+function deleteCompany(req, res, next) {}
+
+function editCompany(req, res, next) {}
--- handlers/jobs.js
+++ handlers/jobs.js
@@ -1,1 +1,18 @@
-const { Job } = require("../models");
+const { Job, Company } = require("../models");
+
+const Validator = require("jsonschema").Validator;
+const v = new Validator();
+
+function readJobPostings(req, res, next) {}
+
+function createJobPosting(req, res, next) {}
+
+function newJobPostingForm(req, res, next) {}
+
+function readJobPosting(req, res, next) {}
+
+function updateJobPosting(req, res, next) {}
+
+function deleteJobPosting(req, res, next) {}
+
+function editJobPosting(req, res, next) {}
