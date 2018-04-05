a2b3cedce0119a9cec8a9d98fb696d6d586eb8b7
Miranda Howitt <miranda.howitt@gmail.com>
2018-04-04 18:28:32 -0700

    user and job handlers testing

--- handlers/users.js
+++ handlers/users.js
@@ -5,8 +5,9 @@ const { newUserSchema } = require("../schemas");
 
 function createUser(req, res, next) {
   const result = v.validate(req.body, newUserSchema);
-  if (!result.isValid) {
-    return next(result.errorMessage);
+  if (!result.valid) {
+    const errors = result.errors.map(e => e.message).join(", ");
+    return next({ message: errors });
   }
   User.createUser(new User(req.body))
     .then(user => {
@@ -21,9 +22,12 @@ function readUsers(req, res, next) {
   });
 }
 
-function newUserForm(req, res, next) {}
+function newUserForm(req, res, next) {
+  res.json({ message: "userForm successfully brought" });
+}
 
 function readUser(req, res, next) {
+  //Checked
   return User.findById(req.params.userId)
     .populate("currentCompany")
     .exec()
@@ -33,7 +37,7 @@ function readUser(req, res, next) {
           .status(404)
           .json({ message: `User ${req.params.userId} not found.` });
       }
-      return res.json(user);
+      return res.json({ data: user });
     })
     .catch(err => {
       return res.json(err);
@@ -43,7 +47,7 @@ function readUser(req, res, next) {
 function updateUser(req, res, next) {
   return User.findByIdAndUpdate(req.params.userId, req.body, {
     new: true
-  }).then(user => res.json(user));
+  }).then(user => res.json({ data: user }));
 }
 
 function deleteUser(req, res, next) {
--- models/job.js
+++ models/job.js
@@ -6,7 +6,7 @@ const jobSchema = new mongoose.Schema(
     title: String,
     company: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: 'Company'
+      ref: "Company"
     },
     salary: Number,
     equity: Number
@@ -20,33 +20,31 @@ jobSchema.statics = {
    * @param {Object} newJobPosting -- an instance of a job document
    */
   createJobPosting(newJobPosting) {
-    return Company.findById(newJobPosting.company)
-      .then(company => {
-        if (!company) throw new Error(`${company} does not exist`);
-        return newJobPosting
-          .save()
-          .then(job =>
-            Company.findOneAndUpdate(job.company, {
-              $addToSet: { jobs: job._id }
-            }).then(() => job)
-          ).catch(err => Promise.reject(err));
-      });
-  }
-};
+    return Company.findById(newJobPosting.company).then(company => {
+      if (!company) throw new Error(`Company does not exist`);
+      return newJobPosting
+        .save()
+        .then(job =>
+          Company.findOneAndUpdate(job.company, {
+            $addToSet: { jobs: job._id }
+          }).then(() => job)
+        )
+        .catch(err => Promise.reject(err));
+    });
+  },
 
-jobSchema.statics = {
   /**
    * As a registered company, delete an existing job posting for your company.
    * @param {String} jobId -- an id corresponding to an existing job postin
    */
   deleteJobPosting(jobId) {
-    return this
-    .findOneAndRemove(jobId)
-    .then(job => 
-      Company.findOneAndUpdate(job.company, { 
-        $pull: { jobs: job._id }
-      }).then(() => job)
-    ).catch(err => Promise.reject(err));
+    return this.findOneAndRemove(jobId)
+      .then(job =>
+        Company.findOneAndUpdate(job.company, {
+          $pull: { jobs: job._id }
+        }).then(() => job)
+      )
+      .catch(err => Promise.reject(err));
   }
 };
 
