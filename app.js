9f4c278571a8fd76010ebe48ac17a755793c58aa
Miranda Howitt <miranda.howitt@gmail.com>
2018-04-10 16:28:43 -0700

    heroku

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const {
   authUserRouter
 } = require("./routers");
 
-const PORT = 3000;
+const PORT = process.env.port || 3000;
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
